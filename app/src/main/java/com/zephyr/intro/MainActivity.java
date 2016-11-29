26d425a8fbed1bd36bd9740df76bbe2f3452d2f9
Ritayan Chakraborty <ritayanout@gmail.com>
2016-11-29 08:54:22 +0530

    Add a sample slide and fix font size

--- app/src/main/java/com/zephyr/intro/MainActivity.java
+++ app/src/main/java/com/zephyr/intro/MainActivity.java
@@ -26,9 +26,9 @@ public class MainActivity extends IntroActivity {
                 .build());
 
         addSlide(new SimpleSlide.Builder()
-                .title("Zephyr")
-                .description("Feature placeholder")
-                .image(android.R.drawable.zoom_plate)
+                .title("No more brightness sliders")
+                .description("Just swipe on the statusbar, and change the screen brightness easily.")
+                .layout(R.layout.intro_layout)
                 .background(android.R.color.white)
                 .backgroundDark(R.color.gray)
                 .layout(R.layout.intro_layout)
@@ -37,7 +37,7 @@ public class MainActivity extends IntroActivity {
         addSlide(new SimpleSlide.Builder()
                 .title("Zephyrthug")
                 .description("Zephyrthug is insane")
-                .image(android.R.drawable.zoom_plate)
+                .layout(R.layout.intro_layout)
                 .background(android.R.color.white)
                 .backgroundDark(R.color.gray)
                 .layout(R.layout.intro_layout)
@@ -46,7 +46,7 @@ public class MainActivity extends IntroActivity {
         addSlide(new SimpleSlide.Builder()
                 .title("Zephyr")
                 .description("ZephyrRox")
-                .image(android.R.drawable.zoom_plate)
+                .layout(R.layout.intro_layout)
                 .background(android.R.color.white)
                 .backgroundDark(R.color.gray)
                 .layout(R.layout.intro_layout)
@@ -55,10 +55,9 @@ public class MainActivity extends IntroActivity {
         addSlide(new SimpleSlide.Builder()
                 .title("Zephyr")
                 .description("Desc")
-                .image(android.R.drawable.zoom_plate)
+                .layout(R.layout.intro_layout)
                 .background(android.R.color.white)
                 .backgroundDark(R.color.gray)
-                .layout(R.layout.intro_layout)
                 .build());
 
         //autoplay(2500, INFINITE);
